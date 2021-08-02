90bac2281f5bb890f1b996fb0d3698610b985f45
ramonmarco <juan.ramon.marco.pastor@coverwallet.com>
2021-08-02 11:01:43 +0200

    Initial commit

--- src/checker/salesforce_checker.py
+++ src/checker/salesforce_checker.py
@@ -1,40 +1,30 @@
-import json
 import os
 from datetime import date
-from time import sleep
 
 import pandas as pd
-from salesforce_bulk import SalesforceBulk
-from salesforce_bulk.util import IteratorBytesIO
+from simple_salesforce import Salesforce
 
 
 class SalesforceBulkQuery:
 
     def __init__(self, instance: str):
         if instance == "origin":
-            self.bulk = SalesforceBulk(username=os.getenv("SF_D2C_BETA_USERNAME"),
-                                       password=os.getenv("SF_D2C_BETA_PASSWORD"),
-                                       security_token=os.getenv("SF_D2C_BETA_SECURITY_TOKEN"),
-                                       client_id=os.getenv("SF_D2C_BETA_CLIENT_ID"),
-                                       domain="test"
-                                       )
+            self.sf = Salesforce(username=os.getenv("SF_D2C_BETA_USERNAME"),
+                                 password=os.getenv("SF_D2C_BETA_PASSWORD"),
+                                 security_token=os.getenv("SF_D2C_BETA_SECURITY_TOKEN"),
+                                 client_id=os.getenv("SF_D2C_BETA_CLIENT_ID"),
+                                 domain="test"
+                                 )
         else:
-            self.bulk = SalesforceBulk(username=os.getenv("SF_AON_US_BETA_USERNAME"),
-                                       password=os.getenv("SF_AON_US_BETA_PASSWORD"),
-                                       security_token=os.getenv("SF_AON_US_BETA_SECURITY_TOKEN"),
-                                       client_id=os.getenv("SF_AON_US_BETA_CLIENT_ID"),
-                                       domain="test"
-                                       )
+            self.sf = Salesforce(username=os.getenv("SF_AON_US_BETA_USERNAME"),
+                                 password=os.getenv("SF_AON_US_BETA_PASSWORD"),
+                                 security_token=os.getenv("SF_AON_US_BETA_SECURITY_TOKEN"),
+                                 client_id=os.getenv("SF_AON_US_BETA_CLIENT_ID"),
+                                 domain="test"
+                                 )
 
     def query(self, object_name: str, start_date: date = None, end_date: date = None):
-        job = self.bulk.create_query_job(object_name, contentType='JSON')
-        batch = self.bulk.query(job, f"SELECT Id, Phone FROM {object_name} LIMIT 10")
-        self.bulk.close_job(job)
-        while not self.bulk.is_batch_done(batch):
-            sleep(10)
-
-        for result in self.bulk.get_all_results_for_query_batch(batch):
-            result = json.load(IteratorBytesIO(result))
-            for row in result:
-                print(row)
-        return pd.DataFrame(result)
+        results = self.sf.bulk.Account.query(f"SELECT Id, Phone FROM {object_name} LIMIT 10")
+        for result in results:
+            print(result)
+        return pd.DataFrame(results)
--- src/main.py
+++ src/main.py
@@ -20,4 +20,5 @@ def run_check(object_name: str, start_date: date = None, end_date: date = None):
 
 if __name__ == '__main__':
     kwargs = cli.cli()
-    run_check(**kwargs)
+    result = run_check(**kwargs)
+    print(result)
